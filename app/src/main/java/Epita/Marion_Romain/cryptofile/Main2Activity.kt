4c058ebd67cda953629a148bf83ff9c6f9f1d1ae
romain-yohan <romain.yohan@live.fr>
2020-01-15 14:52:26 +0100

    begin list file display and interaction mecanism

--- app/src/main/java/Epita/Marion_Romain/cryptofile/Main2Activity.kt
+++ app/src/main/java/Epita/Marion_Romain/cryptofile/Main2Activity.kt
@@ -5,18 +5,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity(), View.OnClickListener {
 
     // Activité ou on scroll les fichiers existants et ou on en ajoute de nouveaux.
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        addfilebutton.setOnClickListener(this@Main2Activity)
-    }
 
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = MyAdapter(myDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
 
     override fun onClick(v: View?) {
         if (v!= null && v.id == R.id.addfilebutton) {
